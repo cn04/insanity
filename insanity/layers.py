03f43cbd35c3d9f394f26c2f45534eb0886dc43d
Aedan Cullen <asc.cn04@gmail.com>
2015-10-19 20:35:00 -0400

    Move import statements for dropout to the 'layers' module.

--- insanity/layers.py
+++ insanity/layers.py
@@ -3,3 +3,5 @@ import theano
 import theano.tensor as T
 from theano.tensor.nnet import conv
 from theano.tensor.nnet import softmax
+from theano.tensor import shared_randomstreams
+from theano.tensor.signal import downsample
--- insanity/util.py
+++
@@ -1,4 +0,0 @@
-import theano
-import theano.tensor as T
-from theano.tensor import shared_randomstreams
-from theano.tensor.signal import downsample
