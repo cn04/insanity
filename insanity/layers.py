9b2ee2e4a956f8409047b289ee5c35ad4fcb4310
Aedan Cullen <asc.cn04@gmail.com>
2015-10-31 08:19:19 -0400

    Add @property for network layer inputs

--- insanity/layers.py
+++ insanity/layers.py
@@ -8,22 +8,18 @@ from theano.tensor.signal import downsample
 
 
 
-class NeuralNetworkLayer(object):
+class Layer(object):
 
-	def __init__(self, numInputs, input, inputDropout, numNeurons, activation, miniBatchSize, dropoutAmount=0.0):
+	def __init__(self, numInputs, numNeurons, activation):
 		self.numInputs = numInputs
-		self.input = input
-		self.inputDropout = inputDropout
 		self.numNeurons = numNeurons
 		self.activation = activation
-		self.miniBatchSize = miniBatchSize
-		self.dropoutAmount = dropoutAmount
 		
 		#Initialize weights.
 		self.weights = theano.shared(
             np.asarray(
                 np.random.normal(
-                    loc=0.0, scale=np.sqrt(1.0/n_out), size=(self.numInputs, self.numNeurons)),
+                    loc=0.0, scale=np.sqrt(1.0/self.numNeurons), size=(self.numInputs, self.numNeurons)),
                 dtype=theano.config.floatX),
             name='weights', borrow=True)
             
@@ -32,20 +28,17 @@ class NeuralNetworkLayer(object):
             np.asarray(
 				np.random.normal(
 					loc=0.0, scale=1.0, size=(self.numNeurons,)),
-                dtype=theano.config.floatX),
+				dtype=theano.config.floatX),
             name='biases', borrow=True)
-            
-        #Store parameters to be learned in an attribute so that they can be externally accessed.
-        self.learningParams = [self.weights, self.biases]
         
-        #Define layer outputs.
-        self.output, self.outputDropout = self.configureProcessing(
-			self.input, self.inputDropout, self.weights, self.biases, self.miniBatchSize, self.dropoutAmount)
+    @property
+    def input(value):
+		#Configure the layer output.
+		self.output = something
 
 
 
-class FullyConnectedLayer(NeuralNetworkLayer):
+class FullyConnectedLayer(Layer):
 	
-	def configureProcessing(input, inputDropout, weights, biases, miniBatchSize, dropoutAmount):
-        # TODO things go here
-        return output, outputDropout
+	
+		
