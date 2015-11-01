d6a2e4c09fbf2e6fc287e5b3fe3f6472d217da34
Aedan Cullen <asc.cn04@gmail.com>
2015-11-01 16:09:13 -0500

    Add beginnings of NeuralNetwork class

---
+++ insanity/learn.py
@@ -0,0 +1,13 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+
+
+class NeuralNetwork(object):
+
+    def __init__(self, input, output, learningWeights, learningBiases, miniBatchSize):
+		pass
+		
+	def train(self, trainingInput, trainingOutput, learningRate, lmbda=0.0):
+		pass
